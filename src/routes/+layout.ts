1cc562055141504471e7f5f8164d5623725d5bc8
ewsite <rodel.rizalde09@gmail.com>
2023-12-14 19:03:49 +0800

    Revert back to generic app name :)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,8 @@ import type { LayoutLoad } from './$types'
 export const load: LayoutLoad = async () => {
 	return {
 		meta: {
-			title: 'Oww Zone Vape Shop'
+			// Your company title here
+			title: 'Online Ordering System'
 		}
 	}
 }
